1e64610ae1d5d7a6ee2fed673c9058618563e034
Foxy Socks <foxysocks1010@gmail.com>
2024-02-14 21:13:34 +0000

    Added mongo drivers and DB connections.

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import Express from 'express';
+import Mongoose from 'mongoose';
+import DotEnv from 'dotenv';
+
+DotEnv.config();
+
+Mongoose.connect(process.env.CONNECTION_STRING)
+    .then(() => {
+        console.log('DB connected!!');
+    })
+    .catch((err) => {
+        console.log(err);  
+    })
 
 const App = Express();
 
