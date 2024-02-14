aafe83220666bf70d43f452f53484046d1ecb79e
Foxy Socks <foxysocks1010@gmail.com>
2024-02-14 21:25:05 +0000

    Created user model.

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import Mongoose from "mongoose";
+
+const userSchema = new Mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+    }, { timeStamp: true }
+)
+
+const User = Mongoose.model('User', userSchema);
+
+export default User;
