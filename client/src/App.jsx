a61b86eb10c2dacd54cb00eff24b2e8cc3cf3524
Foxy Socks <foxysocks1010@gmail.com>
2024-02-15 17:18:07 +0000

    Created footer component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Header from './components/header';
+import Footer from './components/footer';
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
             <Route path='/sign-in' element={ <SignIn /> }/>
             <Route path='/sign-up' element={ <SignUp /> }/>
         </Routes>
+        <Footer />
     </BrowserRouter>
   )
 }
